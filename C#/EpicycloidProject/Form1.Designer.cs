fb979835f0fa293f543e151646122bd89e3a0548
gurev vladislav <gurev@satel.org>
2025-09-08 14:44:33 +0300

    C# lab 1

---
+++ C#/EpicycloidProject/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace EpicycloidProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxGraph = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxA = new System.Windows.Forms.TextBox();
+            this.buttonDraw = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxB = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGraph)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxGraph
+            // 
+            this.pictureBoxGraph.Location = new System.Drawing.Point(-3, 0);
+            this.pictureBoxGraph.Name = "pictureBoxGraph";
+            this.pictureBoxGraph.Size = new System.Drawing.Size(804, 374);
+            this.pictureBoxGraph.TabIndex = 0;
+            this.pictureBoxGraph.TabStop = false;
+            this.pictureBoxGraph.Click += new System.EventHandler(this.pictureBoxGraph_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(87, 394);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Параметр a:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBoxA
+            // 
+            this.textBoxA.Location = new System.Drawing.Point(163, 394);
+            this.textBoxA.Name = "textBoxA";
+            this.textBoxA.Size = new System.Drawing.Size(100, 20);
+            this.textBoxA.TabIndex = 2;
+            // 
+            // buttonDraw
+            // 
+            this.buttonDraw.Location = new System.Drawing.Point(641, 411);
+            this.buttonDraw.Name = "buttonDraw";
+            this.buttonDraw.Size = new System.Drawing.Size(147, 23);
+            this.buttonDraw.TabIndex = 3;
+            this.buttonDraw.Text = "Построить график";
+            this.buttonDraw.UseVisualStyleBackColor = true;
+            this.buttonDraw.Click += new System.EventHandler(this.buttonDraw_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(87, 421);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Параметр b:";
+            // 
+            // textBoxB
+            // 
+            this.textBoxB.Location = new System.Drawing.Point(163, 420);
+            this.textBoxB.Name = "textBoxB";
+            this.textBoxB.Size = new System.Drawing.Size(100, 20);
+            this.textBoxB.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBoxB);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.buttonDraw);
+            this.Controls.Add(this.textBoxA);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBoxGraph);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGraph)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxGraph;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxA;
+        private System.Windows.Forms.Button buttonDraw;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxB;
+    }
+}
+
---
+++ C#/EpicycloidProject/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace EpicycloidProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonDraw_Click(object sender, EventArgs e)
+        {
+            double a, b;
+            if (!double.TryParse(textBoxA.Text, out a) || !double.TryParse(textBoxB.Text, out b))
+            {
+                MessageBox.Show("Пожалуйста, введите корректные числовые значения для параметров a и b.", "Ошибка ввода", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            Bitmap bmp = new Bitmap(pictureBoxGraph.Width, pictureBoxGraph.Height);
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                g.Clear(Color.White);
+
+                float scale = (float)Math.Min(pictureBoxGraph.Width, pictureBoxGraph.Height) / (2.5f * (float)(a + b));
+                int xOffset = pictureBoxGraph.Width / 2;
+                int yOffset = pictureBoxGraph.Height / 2;
+
+                Pen axisPen = new Pen(Color.Black, 1);
+                g.DrawLine(axisPen, xOffset, 0, xOffset, pictureBoxGraph.Height);
+                g.DrawLine(axisPen, 0, yOffset, pictureBoxGraph.Width, yOffset);
+
+                Pen curvePen = new Pen(Color.Blue, 2);
+                PointF previousPoint = PointF.Empty;
+                int steps = 1000;
+                double b_over_a = b / a;
+
+                double t_max = (Math.Abs(b_over_a - Math.Round(b_over_a)) < 0.0001) ? 2 * Math.PI : 2 * Math.PI * 250;
+
+                for (int i = 0; i <= steps; i++)
+                {
+                    double t = i * t_max / steps;
+
+                    double x = (a + b) * Math.Cos(t) - a * Math.Cos((a + b) * t / a);
+                    double y = (a + b) * Math.Sin(t) - a * Math.Sin((a + b) * t / a);
+
+                    float x_screen = (float)x * scale + xOffset;
+                    float y_screen = (float)y * scale + yOffset;
+
+                    PointF currentPoint = new PointF(x_screen, y_screen);
+
+                    if (i > 0)
+                    {
+                        g.DrawLine(curvePen, previousPoint, currentPoint);
+                    }
+                    previousPoint = currentPoint;
+                }
+            }
+            pictureBoxGraph.Image = bmp;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBoxGraph_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
