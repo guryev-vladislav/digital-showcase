adfd60d48d27b02f3bf0de1db024aefee35f7aa0
gurev <gurev@satel.org>
2025-10-03 18:03:25 +0300

    Fix habr

--- golang/lib/habr_carrer/config.go
+++ golang/lib/habr_carrer/config.go
@@ -4,7 +4,7 @@ package habr_carrer
 const endpointCreateToken = "/integrations/oauth/token?client_id={%s}&client_secret={%s}&redirect_uri={%s}&grant_type=authorization_code&code={%s}"
 
 // client_id, redirect_uri
-const endpointAuthorize = "/integrations/oauth/authorize?client_id={%s}&redirect_uri={%s}&response_type=code"
+const EndpointAuthorize = "/integrations/oauth/authorize?client_id={%s}&redirect_uri={%s}&response_type=code"
 
 const endpointGetUser = "/v1/integrations/users/me"
 const endpointGetVacancies = "/v1/integrations/vacancies"
